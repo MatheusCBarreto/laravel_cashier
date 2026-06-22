474677da4b07cd13b054b5bf89a138d8909f3434
MatheusCBarreto <devmattbarreto@protonmail.com>
2026-06-22 20:47:41 -0300

    criação e organização do projeto

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,21 +5,23 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    use WithoutModelEvents;
-
     /**
      * Seed the application's database.
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        for ($id = 1; $id < 10; $id++) {
+            DB::table('users')->insert([
+                "name" => "User $id",
+                "email" => "user_$id@teste.com",
+                "password" => bcrypt("user$id"),
+                "email_verified_at" => now(),
+                "created_at" => now()
+            ]);
+        }
     }
 }
